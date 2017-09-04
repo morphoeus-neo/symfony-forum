5f313dfba19b5fe14072bce7767e4969935f11c5
Sébastien Maloron <smaloron@moi.com>
2017-09-04 12:35:25 +0200

    formulaire pour ajouter des thèmes

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Theme;
+use AppBundle\Form\ThemeType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,13 +21,34 @@ class AdminController extends Controller
      * @Route("/themes", name="admin_themes")
      * @return Response
      */
-    public function themeAction(){
+    public function themeAction(Request $request){
         $repository = $this->getDoctrine()
             ->getRepository("AppBundle:Theme");
 
         $themeList = $repository->findAll();
 
-        return $this->render("admin/theme.html.twig", ["themeList" => $themeList]);
+        //Génération du formulaire
+        $theme = new Theme();
+        $form = $this->createForm(ThemeType::class, $theme);
+
+        //hydratation de l'entité
+        $form->handleRequest($request);
+
+        //Traitement du formulaire
+        if($form->isSubmitted() and $form->isValid()){
+            //Persistance de l'entité
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($theme);
+            $em->flush();
+
+            //Redirection pour éviter de poster deux fois les données
+            return $this->redirectToRoute("admin_themes");
+        }
+
+        return $this->render("admin/theme.html.twig", [
+            "themeList" => $themeList,
+            "themeForm" => $form->createView()
+        ]);
     }
 
 }
